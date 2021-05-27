fa05606c8cb3229ee72a5c76186b1fdbf06c2983
Princetripathi087 <69450389+Princetripathi087@users.noreply.github.com>
2021-05-27 16:56:38 +0530

    Add files via upload

---
+++ shopping/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Mobilep,Elec,Home,Homedeal,Mobilesp,Indexs,Elecs,Featspc,Product
+# Register your models here.
+admin.site.register(Mobilep)
+admin.site.register(Elec)
+admin.site.register(Home)
+admin.site.register(Homedeal)
+admin.site.register(Mobilesp)
+admin.site.register(Indexs)
+admin.site.register(Elecs)
+admin.site.register(Featspc)
+admin.site.register(Product)
